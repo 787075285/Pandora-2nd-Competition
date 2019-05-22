6cb816c062f5a66a0513fb8003d58f5aa3d44577
787075285 <50857927+787075285@users.noreply.github.com>
2019-05-22 23:27:08 +0800

    Update __init__.py

--- pandora/__init__.py
+++ pandora/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
+from flask import render_template
+import requests
+from lxml import etree
+import json
+from flask import request
 
 
 def create_app():
@@ -6,10 +11,6 @@ def create_app():
 
     @app.route('/')
     def index():
-        """
-        只有 Hello World 的首页
-        :return:
-        """
         return "Hello, world!"
 
     # TODO: 捕获 404 错误，返回 404.html
@@ -18,12 +19,15 @@ def create_app():
         """
         以此项目中的404.html作为此Web Server工作时的404错误页
         """
-        pass
+        return render_template('404.html',name=name),404
+        
 
     # TODO: 完成接受 HTTP_URL 的 picture_reshape
     # TODO: 完成接受相对路径的 picture_reshape
     @app.route('/pic', methods=['GET'])
     def picture_reshape():
+        if request.method == 'GET':
+            show_the_login_form()
         """
         **请使用 PIL 进行本函数的编写**
         获取请求的 query_string 中携带的 b64_url 值
@@ -59,6 +63,35 @@ def create_app():
             "description": <description 描述>
         }, ...]
         """
-        pass
+        html = requests.get("https://github.com/996icu/996.ICU/blob/master/blacklist/README.md")
+        # print html.text
+        etree_html = etree.HTML(html.text)
+        content1 = etree_html.xpath('//*[@id="readme"]/article/table[2]/tbody/tr[*]/td[1]/text()')
+        content2 = etree_html.xpath('//*[@id="readme"]/article/table[2]/tbody/tr[*]/td[2]/a/text()')
+        content2a = etree_html.xpath('//*[@id="readme"]/article/table[2]/tbody/tr[*]/td[2]/text()')
+        content2 +=content2a
+        content3 = etree_html.xpath('//*[@id="readme"]/article/table[2]/tbody/tr[*]/td[3]/text()')
+        content4 = etree_html.xpath('//*[@id="readme"]/article/table[2]/tbody/tr[*]/td[4]/text()')
+        content4[102]+=content4[103]
+        del content4[103]
+        i = len(content1)
+        j = 0
+        list1=[]
+        while i >j:
+            data={}
+            data["city"]=content1[j]
+            data["company"]=content2[j]
+            data["exposure_time"]=content3[j]
+            data["description"]=content4[j]
+            list1.append(data)
+            j+=1
+        return json.dumps(list1)
+
+        
 
     return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
